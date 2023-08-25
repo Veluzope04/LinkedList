95fb839dd168be8058b3967972ebd3b3c06647bf
velu <veluzope776@gmail.com>
2023-08-25 22:30:48 +0530

    Linked List

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        linkedlist l1 = new linkedlist();
+
+
+        l1.add("AA");
+        l1.add("BB");
+        l1.addfromfirst("DD");
+        l1.display();
+        l1.addOnposition("FF",2);
+        System.out.println();
+        l1.display();
+
+    }
+}
